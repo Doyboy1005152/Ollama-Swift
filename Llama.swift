ddd16398242cc4c03122b2610f1812b1d2f9d772
Doyboy1005152 <gorges.enigma_01@icloud.com>
2025-03-12 16:08:14 -0400

    Add files via upload

---
+++ Llama.swift
@@ -0,0 +1,34 @@
+import Foundation
+import SwiftUI
+
+@available(macOS 10.15, *)
+public func askLlama(_ text: String, _ translation: Binding<String>, _ otherInfo: String = "") async {
+    let command = "echo 'You are a helpful assistant who follows these instructions: \"\(otherInfo)\". Do not include any formatting (besides newlines). Make sure yout sentence(s) maintain clarity. They are saying \"\(text)\"' | /usr/local/bin/ollama run mistral"
+    
+    let process = Process()
+    process.launchPath = "/bin/bash"
+    process.arguments = ["-c", command]
+
+    let pipe = Pipe()
+    process.standardOutput = pipe
+
+    if #available(macOS 10.15, *) {
+        await withCheckedContinuation { continuation in
+            DispatchQueue.global(qos: .background).async {
+                process.launch()
+                process.waitUntilExit()
+                
+                let data = pipe.fileHandleForReading.readDataToEndOfFile()
+                if let output = String(data: data, encoding: .utf8) {
+                    let translationResult = output.trimmingCharacters(in: .whitespacesAndNewlines)
+                    DispatchQueue.main.async {
+                        translation.wrappedValue = translationResult
+                    }
+                }
+                continuation.resume()
+            }
+        }
+    } else {
+        // Fallback on earlier versions
+    }
+}
