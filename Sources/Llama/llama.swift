5e1d58ab8ebec84c186cd2acc2cce2afeca084e6
Doyboy1005152 <gorges.enigma_01@icloud.com>
2025-03-12 17:40:09 -0400

    Update llama.swift

--- Sources/Llama/llama.swift
+++ Sources/Llama/llama.swift
@@ -1,10 +1,12 @@
 import Foundation
 import SwiftUI
 
+#if os(macOS)
 @available(macOS 10.15, *)
 public func askLlama(_ text: String, _ translation: Binding<String>, _ otherInfo: String = "") async {
-    let command = "echo 'You are a helpful assistant who follows these instructions: \"\(otherInfo)\". Do not include any formatting (besides newlines). Make sure yout sentence(s) maintain clarity. They are saying \"\(text)\"' | /usr/local/bin/ollama run \(modelName)"
     let modelName = "mistral"
+    let command = "echo 'You are a helpful assistant who follows these instructions: \"\(otherInfo)\". Do not include any formatting (besides newlines). Make sure your sentence(s) maintain clarity. They are saying \"\(text)\"' | /usr/local/bin/ollama run \(modelName)"
+    
     let process = Process()
     process.launchPath = "/bin/bash"
     process.arguments = ["-c", command]
@@ -12,23 +14,20 @@ public func askLlama(_ text: String, _ translation: Binding<String>, _ otherInfo
     let pipe = Pipe()
     process.standardOutput = pipe
 
-    if #available(macOS 10.15, *) {
-        await withCheckedContinuation { continuation in
-            DispatchQueue.global(qos: .background).async {
-                process.launch()
-                process.waitUntilExit()
-                
-                let data = pipe.fileHandleForReading.readDataToEndOfFile()
-                if let output = String(data: data, encoding: .utf8) {
-                    let translationResult = output.trimmingCharacters(in: .whitespacesAndNewlines)
-                    DispatchQueue.main.async {
-                        translation.wrappedValue = translationResult
-                    }
+    await withCheckedContinuation { (continuation: CheckedContinuation<Void, Never>) in
+        DispatchQueue.global(qos: .background).async {
+            process.launch()
+            process.waitUntilExit()
+            
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            if let output = String(data: data, encoding: .utf8) {
+                let translationResult = output.trimmingCharacters(in: .whitespacesAndNewlines)
+                DispatchQueue.main.async {
+                    translation.wrappedValue = translationResult
                 }
-                continuation.resume()
             }
+            continuation.resume()
         }
-    } else {
-        // Fallback on earlier versions
     }
 }
+#endif
